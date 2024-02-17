b6e99c80e34f15db2f12474c36090297aaf66eaf
schreda <strieder.georg@gmail.com>
2024-02-17 22:46:16 +0100

    :arrow_up: Added collision detection area2d example

---
+++ collision-detection/rust/src/player.rs
@@ -0,0 +1,71 @@
+use godot::engine::global::Key;
+use godot::engine::{InputEvent, IArea2D, Area2D};
+use godot::prelude::*;
+use std::collections::HashMap;
+
+#[derive(GodotClass)]
+#[class(base=Area2D)]
+pub struct Player {
+    base: Base<Area2D>,
+    keys: Vec<Key>,
+    key_states: HashMap<Key, bool>,
+}
+
+#[godot_api]
+impl IArea2D for Player {
+    fn init(base: Base<Area2D>) -> Self {
+        Self {
+            base,
+            keys: vec![Key::LEFT, Key::RIGHT, Key::UP, Key::DOWN],
+            key_states: HashMap::new(),
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        let radians = (3.14 * delta) as f32;
+        self.base_mut().rotate(radians);
+        let mut current_position = self.base_mut().get_position();
+
+        self.keys.iter().for_each(|k| {
+            match self.key_states.get(&k) {
+                Some(true) => {}
+                _ => return,
+            };
+
+            match *k {
+                Key::UP => current_position.y -= 1.0 * 1000.0 * delta as f32,
+                Key::DOWN => current_position.y += 1.0 * 1000.0 * delta as f32,
+                Key::LEFT => current_position.x -= 1.0 * 1000.0 * delta as f32,
+                Key::RIGHT => current_position.x += 1.0 * 1000.0 * delta as f32,
+                _ => {
+                    godot_print!("key not supported {:?}", k);
+                }
+            };
+        });
+
+        self.base_mut().set_position(current_position);
+    }
+
+    fn input(&mut self, _event: Gd<InputEvent>) {
+        self.keys.iter().for_each(|k| {
+            let is_key_pressed = Input::singleton().is_key_pressed(*k);
+            self.key_states.insert(*k, is_key_pressed);
+        });
+    }
+}
+
+#[godot_api]
+impl Player {
+
+    #[func]
+    fn did_collide_with(_area: Gd<Area2D>) {
+        godot_print!("colliding with object");
+    }
+
+    #[func]
+    fn did_exit_collision(_area: Gd<Area2D>) {
+        godot_print!("collision will exit");
+    }
+
+}
+
