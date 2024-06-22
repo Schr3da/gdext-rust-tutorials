82f9f92536daab43f33dfde6f669fc1c35d3f013
Schreda <deck@steamdeck.fritz.box>
2024-06-22 22:25:39 +0200

    🚨 Fixed build errors

--- ecs/rust/src/nodes/main_menu_node.rs
+++ ecs/rust/src/nodes/main_menu_node.rs
@@ -1,43 +1,39 @@
-use godot::prelude::*;
 use crate::nodes::prelude::*;
+use godot::prelude::*;
 
 #[derive(GodotClass)]
-#[class(base=Node)]
+#[class(base=Node2D)]
 pub struct MainMenuNode {
-    base: Base<Node>,
+    base: Base<Node2D>,
 }
 
-#[godot_api] 
+#[godot_api]
 impl INode2D for MainMenuNode {
-    fn init(base: Base<Node>) -> Self {
-        Self {
-            base,
-        }
+    fn init(base: Base<Node2D>) -> Self {
+        Self { base }
     }
 }
 
 #[godot_api]
-impl MainMenuNode{
-
+impl MainMenuNode {
     #[func]
     pub fn new_game_pressed(&mut self) {
         let parent = match self.base().get_parent() {
             Some(p) => p,
             None => {
                 godot_error!("unable to find parent");
-                    return;
-            },
+                return;
+            }
         };
 
-        let mut node =  match parent.try_cast::<EntryNode>() {
+        let mut node = match parent.try_cast::<EntryNode>() {
             Err(_) => {
                 godot_error!("Unable to cast to Entry Node");
-                    return;
-            },
+                return;
+            }
             Ok(n) => n,
         };
 
         node.bind_mut().new_game();
-
     }
 }
